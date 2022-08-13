924b7b46c9a99b5f625a8e96b6bf80f6fcb9ed97
sebronas <sebriskristaps@gmail.com>
2022-08-13 10:17:00 +0300

    [init] - project skeleton

---
+++ src/main/java/com/sda/travelagency/TravelAgencyApplication.java
@@ -0,0 +1,13 @@
+package com.sda.travelagency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAgencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelAgencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sda/travelagency/TravelAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.travelagency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAgencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
