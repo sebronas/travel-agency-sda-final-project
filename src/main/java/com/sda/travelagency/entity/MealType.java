48a3cf8cf5e35e94060a0b4e64305cefb9723057
sebronas <sebriskristaps@gmail.com>
2022-08-14 09:16:40 +0300

    [change] - trip entity in progress

--- src/main/java/com/sda/travelagency/entity/MealType.java
+++ src/main/java/com/sda/travelagency/entity/MealType.java
@@ -1,13 +1,13 @@
 package com.sda.travelagency.entity;
 
 public enum MealType {
-    BREAKFAST(""),
-    LUNCH(""),
-    DINNER(""),
-    SNACKS(""),
-    DRINKS(""),
+    BREAKFAST("Morning food"),
+    LUNCH("Food for the day"),
+    DINNER("Food for the evening"),
+    SNACKS("If you didn't have enough"),
+    DRINKS("Liquid to help you sleep, pick your poison (VODKA/SPIRIT)"),
     HB(""),
-    ALL_INCLUSIVE("");
+    ALL_INCLUSIVE("You get it all!");
 
     private String mealDescription;
 
