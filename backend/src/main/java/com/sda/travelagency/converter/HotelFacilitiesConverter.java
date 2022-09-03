3904cf1f067e639fe1618c02ef89989537035404
sebronas <sebriskristaps@gmail.com>
2022-09-03 10:30:25 +0300

    [change] - HotelFacilitiesConverter - ready / JAVA 7 style :D

--- backend/src/main/java/com/sda/travelagency/converter/HotelFacilitiesConverter.java
+++ backend/src/main/java/com/sda/travelagency/converter/HotelFacilitiesConverter.java
@@ -1,15 +1,35 @@
 package com.sda.travelagency.converter;
 
+import com.sda.travelagency.dto.AttractionDto;
 import com.sda.travelagency.dto.HotelFacilitiesDto;
+import com.sda.travelagency.entity.Attraction;
 import com.sda.travelagency.entity.HotelFacilities;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class HotelFacilitiesConverter implements Converter<HotelFacilities, HotelFacilitiesDto> {
+
+    private final AttractionConverter attractionConverter;
+
+    public HotelFacilitiesConverter(AttractionConverter attractionConverter) {
+        this.attractionConverter = attractionConverter;
+    }
+
     @Override
     public HotelFacilitiesDto fromEntityToDto(HotelFacilities entity) {
         // throw new NotYetImplementedException();    <--- exception if its not finished yet
-        return null;
+
+        // till Java 8 implementation
+        List<AttractionDto> attractionDtos = new ArrayList<>(); // creating empty list for attractionsDto
+        for (Attraction attraction: entity.getAttractionList()) { // for every
+            AttractionDto attractionDto = attractionConverter.fromEntityToDto(attraction); // converting
+            attractionDtos.add(attractionDto); // adding to list
+        }
+
+        return new HotelFacilitiesDto(attractionDtos, entity.getApartmentFacilities()); // putting attractionDtos
     }
 
     @Override
