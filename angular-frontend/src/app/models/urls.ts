25c29f5164d4437184d6cdab79db3ae7ba7fe6c4
sebronas <sebriskristaps@gmail.com>
2022-09-04 10:14:31 +0300

    [refactor] - better way of preparing urls for server connection

--- angular-frontend/src/app/models/urls.ts
+++ angular-frontend/src/app/models/urls.ts
@@ -1,1 +1,7 @@
-const allTrips: string = 'http://localhost:8080/api/trips'
+const serverUrl = 'http://localhost:8080'
+
+// with apostrophe - JAVA way
+const allTripsUrlJavaWay: string = serverUrl +  '/api/trips'
+
+// with backticks to use ${...}
+export const allTripsUrl: string = `${serverUrl}/api/trips`
