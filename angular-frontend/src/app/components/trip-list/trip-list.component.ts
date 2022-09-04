ba680df1d7bc042bc9de32a0c0142f72c1d5dc29
sebronas <sebriskristaps@gmail.com>
2022-09-04 13:45:21 +0300

    [change] - server delay simulation

--- angular-frontend/src/app/components/trip-list/trip-list.component.ts
+++ angular-frontend/src/app/components/trip-list/trip-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {TripService} from "../../services/trip-service/trip.service";
 import {TripDto} from "../../models/trips";
-import {tap} from "rxjs";
+import {delay, tap} from "rxjs";
 
 @Component({
   selector: 'app-trip-list',
@@ -19,8 +19,9 @@ export class TripListComponent implements OnInit {
     // place for querying the backend and obtaining results
     this.tripService.getAllTrip()
       .pipe(
-        tap(value => console.log("from server: " + value))  // tap() is for looping - we can see what's inside the stream of data
-      )      // TODO: delay data to present that they are available some time later
+        delay(1_000), // additional delay to simulate server delay
+        tap(value => console.log("from server: " + JSON.stringify(value)))  // tap() is for looping - we can see what's inside the stream of data
+      )
       .subscribe(value => this.trips = value)
   }
 
