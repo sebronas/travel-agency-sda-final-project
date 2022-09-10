141424cde71462128597238bc51c18fc8fb93bca
sebronas <sebriskristaps@gmail.com>
2022-09-10 11:23:55 +0300

    [change] - adding internal navigation

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -1,17 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { TripListComponent } from './components/trip-list/trip-list.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {TripListComponent} from './components/trip-list/trip-list.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatCardModule} from "@angular/material/card";
-import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {HttpClientModule} from "@angular/common/http";
 import {MatButtonModule} from "@angular/material/button";
-import { HeaderComponent } from './components/header/header.component';
-import { NavigationComponent } from './components/navigation/navigation.component';
+import {HeaderComponent} from './components/header/header.component';
+import {NavigationComponent} from './components/navigation/navigation.component';
 import {MatButtonToggleModule} from "@angular/material/button-toggle";
-import { AboutusComponent } from './components/aboutus/aboutus.component';
-import { HomeComponent } from './components/home/home.component';
+import {AboutusComponent} from './components/aboutus/aboutus.component';
+import {HomeComponent} from './components/home/home.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
@@ -28,9 +29,16 @@ import { HomeComponent } from './components/home/home.component';
     HttpClientModule,
     MatCardModule,
     MatButtonModule,
-    MatButtonToggleModule
+    MatButtonToggleModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent}, // default
+      { path: 'home', component: HomeComponent},
+      { path: 'about-us', component: AboutusComponent},
+      { path: 'all-trips', component: TripListComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
